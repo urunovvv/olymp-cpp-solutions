f72d8e488900c7ff7f7bcb185b0f80f222cbd22e
urunovvv <urunovff18@mail.ru>
2025-12-13 14:13:57 +0500

    Release #131225

--- 192.168.0.150/training_cont_26_06_25/B.cpp
+++ 192.168.0.150/training_cont_26_06_25/B.cpp
@@ -1,14 +1,34 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-using ll = long long;
+
+ll dig_sum(ll n){
+    ll cnt=0;
+    while(n>0){
+        cnt+=n%10;
+        n/=10;
+    }
+    return cnt;
+}
+
+ll solve(ll n){
+    ll cnt=0;
+    while(n>0){
+        n -= dig_sum(n);
+        cnt++;
+    }
+    return cnt;
+}
 
 int main(){
     ll n;
     cin >> n;
-
-    ll x = 1;
-    while (x < n) x *= 10;
-
-    cout << x;
+    ll l=0, r=2e7;
+    while(r - l > 1){
+        ll mid = (l + r) / 2;
+        if(solve(mid) >= n) r = mid;
+        else l = mid;
+    }
+    cout << r;
     return 0;
 }
