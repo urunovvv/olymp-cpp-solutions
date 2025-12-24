377e9d762bcb1d48e938c0218e6adda161fd9d04
urunovvv <urunovff18@mail.ru>
2025-12-24 16:05:23 +0500

    Release #241225

--- acmp_отбор/Contest 17.12.2025/F.cpp
+++ acmp_отбор/Contest 17.12.2025/F.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main(){
-    int n, d;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    ll d;
     cin >> n >> d;
-    vector<pair<int, int>>v(n);
-    for (int i=0; i<n; i++){
-        cin >> v[i].first;
-        v[i].second = i;
+    vector<pair<ll,int>> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].first;
+        a[i].second = i;
     }
-    sort(v.begin(), v.end());
-    vector<int>res(n);
-    int cnt = 1;
-    res[v[0].second] = cnt;
-    for (int i=1; i<n; i++){
-        if(v[i].first - v[i-1].first > d){
-            res[v[i].second] = cnt;
+    sort(a.begin(), a.end());
+    vector<ll> last;
+    vector<int> ans(n, 0);
+    for (auto &p : a) {
+        int found = -1;
+        for (int i = 0; i < last.size(); i++) {
+            if (p.first - last[i] > d) {
+                found = i;
+                break;
+            }
         }
-        else{
-            if (cnt>1) res[v[i].second] = --cnt;
-            else res[v[i].second] = cnt++;
+        if (found == -1) {
+            last.push_back(p.first);
+            ans[p.second] = last.size();
         }
+        else {
+            last[found] = p.first;
+            ans[p.second] = found + 1;
+            }
     }
-    for (auto x:res){cout << x << ' ';}
+    cout << last.size() << "\n";
+    for (auto x:ans) cout << x << ' ';
+    cout << "\n";
     return 0;
 }
