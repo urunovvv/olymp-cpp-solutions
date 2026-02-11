6978ae791675aed14408b5daf700c4899b475d47
urunov <urunovff18@mail.ru>
2026-02-11 11:30:34 +0500

    Changes

--- olymp.tj/9-10_region_2026(round 2)/F.cpp
+++ olymp.tj/9-10_region_2026(round 2)/F.cpp
@@ -1,27 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 void firuz()
 {
-    int n,x,y;
+    ll n;
+    int x, y;
     cin >> n >> x >> y;
-    vector<int>v(x);
-    for (auto &i:v) cin >> i;
-    sort(begin(v), end(v));
-    int cnt = 0;
-    for (int i=1;i<n;i++){
-        if (v[i] - v[i-1] == 2) cnt++;
+    vector<ll> a(x);
+    for (int i = 0; i < x; i++) {
+        cin >> a[i];
     }
-    cnt += v[x-1] - v[0] == 2;
+    sort(a.begin(), a.end());
+    ll cnt = 0;
+    for (int i = 0; i < x; i++) {
+        ll next = (i + 1 < x ? a[i + 1] : a[0] + n);
+        if (next - a[i] == 2) {
+            cnt++;
+        }
+    }
     cnt += x-2;
-    cout << cnt << '\n';
+    cout << cnt << "\n";
 }
 
+
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
-    while (t--){
-        firuz();
-    }
+    while (t--) firuz();
     return 0;
 }
